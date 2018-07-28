775771e4b148e0020884c57bdcba19bf4b35d2fb
Ashok Kumar <ashokbigapp@gmail.com>
2018-07-28 11:13:04 +0000

    production changes

--- includes/constants.php
+++ includes/constants.php
@@ -1,4 +1,4 @@
 <?php
-// define('base_url',"https://www.bigappcompany.com/");
- define('base_url',"https://localhost/bigapp-new/");
+define('base_url',"https://bigappcompany.com/");
+ //define('base_url',"https://localhost");
 ?>
