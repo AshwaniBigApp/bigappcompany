32bd17e1bd4a04bde1737507260419329adee3e3
RakeshMGowda <rakesh@spotsoon.com>
2018-09-17 16:38:36 +0530

    COntact form test

---
+++ tech-solutions/footer.php
@@ -0,0 +1,102 @@
+<section class="bootom p40 bg-green">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="bottom-logo"> <img src="images/logo.png" classs="img-responsive"> </div>
+                <div class="bottom-logo-description">
+                    <p>We’re a team of curious people that share a passion for creating
+digital experiences that generate greater results. </p>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="column">
+                            <ul>
+                                <li><strong>Products</strong></li>
+                                <li><a href="#">Payment Gateway</a></li>
+                                <li><a href="#">Route</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Smart Collect</a></li>
+                                <li><a href="#">Payment Links</a></li>
+                            </ul>
+                            <ul>
+                                <li><strong>More</strong></li>
+                                <li><a href="#">Pricing</a></li>
+                                <li><a href="#">Checkout Demo</a></li>
+                                <li><a href="#">UPI</a></li>                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="column">
+                            <ul>
+                                <li><strong>Products</strong></li>
+                                <li><a href="#">Smart Collect</a></li>
+                                <li><a href="#">Invoices</a></li>
+                                <li><a href="#">Payment Links</a></li>
+                            </ul>
+                            <ul>
+                                <li><strong>More</strong></li>
+                                <li><a href="#">Pricing</a></li>
+                                <li><a href="#">Checkout Demo</a></li>
+                                <li><a href="#">UPI</a></li>
+                                <li><a href="#">ePOS</a></li>
+                                <li><a href="#">eCOD</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="column">
+                            <ul>
+                                <li><strong>Company</strong></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Careers<span class="tag">We're hiring!</span></a></li>
+                                <li><a href="#">T&C</a></li>
+                                <li><a href="#">Privacy</a></li>
+                            </ul>
+                            <ul>
+                                <li><strong>Contact</strong></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="mailto:support@bigappcompany.com">support@bigappcompany.com</a></li>
+                            </ul>
+                            <ul>
+                                <li><strong>Find us online</strong></li>
+                                <li class="social-links">
+                                    <a href="https://www.facebook.com" target="_blank">
+                                        <div class="social facebook"></div>
+                                    </a>
+                                    <a href="https://twitter.com" target="_blank">
+                                        <div class="social twitter"></div>
+                                    </a>
+                                    <a href="https://instagram.com" target="_blank">
+                                        <div class="social instagram"></div>
+                                    </a>
+                                    <a href="https://github.com" target="_blank">
+                                        <div class="social github"></div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul><li><div class="copyright">© Bigappcompany 2018<br>All Rights Reserved</div></li></ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<script src="js/custom.js" type="text/javascript"></script>
+<script src="js/wow.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+     new WOW().init();
+</script>
+
+</body>
+
+</html>
