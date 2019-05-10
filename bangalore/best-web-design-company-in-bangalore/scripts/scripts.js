e39c8a2ce52f1c1478d627950a7865102b32117d
AshwaniBigApp <ashwani@bigappcompany.in>
2019-05-10 19:34:37 +0530

    new website launch

--- bangalore/best-web-design-company-in-bangalore/scripts/scripts.js
+++
@@ -1,3 +0,0 @@
- var win = $(window)
-     , foo = $('#typer');
- foo.typer(['Beautiful Apps', 'Timeless Brand Identities', 'Beautiful Websites']);
--- includes/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('base_url',"https://bigappcompany.com/");
- //define('base_url',"https://localhost");
-?>
